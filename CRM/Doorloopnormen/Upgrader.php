9f983815275948dd7c592960166444464ea1d364
Jaap Jansma <jaap@edeveloper.nl>
2016-11-14 14:55:58 +0100

    Added doorloopnormen voor expert application proces

--- CRM/Doorloopnormen/Upgrader.php
+++ CRM/Doorloopnormen/Upgrader.php
@@ -13,4 +13,12 @@ class CRM_Doorloopnormen_Upgrader extends CRM_Doorloopnormen_Upgrader_Base {
    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS `civicrm_doorloopnormen`");
   }
 
+  public function upgrade_1001() {
+    CRM_Core_DAO::executeQuery("INSERT INTO civicrm_doorloopnormen (name, label, norm) VALUES
+('expert_application_assesment_cv', 'From start Expert application to assesment intake', 14),
+('expert_application_filled_pum_cv', 'From start Expert application to filled out pum cv', 14),
+('expert_application_activation_cv', 'From start Expert application to activation of expert', 14);");
+    return true;
+  }
+
 }
--- sql/doorloopnormen.sql
+++ sql/doorloopnormen.sql
@@ -9,7 +9,10 @@ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 INSERT INTO civicrm_doorloopnormen (name, label, norm) VALUES
-('expert_application', 'From start Expert application to end Expert application', 31),
+('expert_application', 'From start Expert application to end Expert application', 42),
+('expert_application_assesment_cv', 'From start Expert application to assesment intake', 14),
+('expert_application_filled_pum_cv', 'From start Expert application to filled out pum cv', 14),
+('expert_application_activation_cv', 'From start Expert application to activation of expert', 14),
 ('from_request_to_approve_prof', 'From Request To Approve Prof', 10),
 ('from_customer_created_to_request', 'From Customer Created to Request', 1),
 ('from_request_to_approve_rep', 'From Request To Approve Rep', 3),
